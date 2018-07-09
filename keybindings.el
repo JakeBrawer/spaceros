64a2d51913615ef605ccbf16a363618e9b1bcb09
JakeBrawer <jake.brawer@yale.edu>
2018-07-09 16:22:03 -0400

    [keybindings][WiP] Added many new keybindings and groups.

--- keybindings.el
+++ keybindings.el
@@ -1,16 +1,31 @@
 
 (spacemacs/declare-prefix "aR" "ros")
-(spacemacs/set-leader-keys "aRf" 'find-ros-file)
-(spacemacs/set-leader-keys "aRs" 'find-ros-service)
-(spacemacs/set-leader-keys "aRa" 'find-ros-action)
-(spacemacs/set-leader-keys "aRm" 'find-ros-message)
+
+(spacemacs/declare-prefix "aRv" "view")
+(spacemacs/set-leader-keys
+  "aRvh" 'helm-ros
+  "aRvf" 'find-ros-file
+  "aRvs" 'helm-ros-services
+  "aRva" 'helm-ros-actions
+  "aRvm" 'helm-ros-messages
+  "aRvl" 'helm-ros-launchfiles
+  "aRvn" 'helm-ros-rosnode-list)
 
 (spacemacs/set-leader-keys "aRc" 'helm-ros-roscore)
-(spacemacs/set-leader-keys "aRl" 'helm-ros-roslaunch)
 
 (spacemacs/declare-prefix "aRt" "topic")
-(spacemacs/set-leader-keys "aRte" 'echo-ros-topic)
-(spacemacs/set-leader-keys "aRti" 'ros-topic-info)
+(spacemacs/set-leader-keys
+  "aRtt" 'helm-ros-topics
+  "aRtl" 'helm-ros-rostopic-list
+  "aRte" 'helm-ros-rostopic-echo
+  "aRti" 'helm-ros-rostopic-info
+  "aRth" 'helm-ros-rostopic-hz)
 
-(spacemacs/set-leader-keys "aRh" 'helm-ros)
+(spacemacs/declare-prefix "aRr" "run")
+(spacemacs/set-leader-keys
+  "aRrr" 'ros-run
+  "aRrl" 'ros-launch
+  "aRrc" 'ros-launch-current
+  )
+
 (spacemacs/set-leader-keys "aRI" 'helm-ros-invalidate-cache)
